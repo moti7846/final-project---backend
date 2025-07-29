20f8016638b42177293e11439301bfbf99f21d2f
moti7846 <8563816@gmail.com>
2025-07-29 17:30:43 +0300

    feat: add bcrypt for password hashing in authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { UserDTO } from 'src/DTO/user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
     constructor(private usersService: UsersService, private jwtService: JwtService) { }
 
-    signIn(username: string, pass: string,): { message: string, token } | { message: string } {
+    async signIn(username: string, pass: string) {
         const user = this.usersService.getUser(username);
-        if (user?.password !== pass) {
+        if (!user || !await bcrypt.compare(pass, user.password)) {
             return { message: 'Incorrect username or password.' }
         }
         const payload = { name: user.name, role: user.role };
@@ -17,8 +18,8 @@ export class AuthService {
         return { message: 'You have successfully logged in.', token }
     }
 
-    signUp(name: string,role : string ,password: string,): string {
-        const newUser : UserDTO = {name , role ,password   }
+    async signUp(name: string,role : string ,password: string,): Promise<string> {
+        const newUser : UserDTO = {name , role ,password : await bcrypt.hash(password, 10) }
         return this.usersService.addUser(newUser);
     }
 }
